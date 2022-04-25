8d38c732c853a90e755bcf4bed22f82ae820ff1d
uday <udaynl619@gmail.com>
2022-04-25 19:27:32 +0530

    adding flight microservice

---
+++ src/main/java/com/booking/vo/User.java
@@ -0,0 +1,22 @@
+package com.booking.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+	private int userId;
+	private String userName;
+	private String firstName;
+	private String lastName;
+	private String userType;
+	private String email;
+	private String password;
+	private String gender;
+	private int age;
+	private long mobileNo;
+	private String address;
+}
